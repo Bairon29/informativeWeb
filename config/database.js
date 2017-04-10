7ece188385067fe58f153d28b4568f0b6eb1e868
Bairon Vasquez <baironvasquez29@gmail.com>
2017-04-10 14:06:07 -0400

    sign up/ sign in feature with mongodb

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'url': 'mongodb://localhost/YOUR_HISTORY'
+}
+// process.env.MONGOLAB_URI ||
+//   process.env.MONGOHQ_URL ||
--- server.js
+++ server.js
@@ -5,26 +5,35 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var flash = require('connect-flash');
 
+// var mustacheExpress = require('mustache-express');
 var methodOver = require('method-override');
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
-// var configDB = require('./config/database.js');
+var configDB = require('./config/database.js');
 
-// mongoose.connect(configDB.url);
+mongoose.connect(configDB.url);
+
+require('./config/passport.js')(passport);
 
 app.use(morgan('dev'));
 app.use(cookieParser());
 
+// app.engine('html', mustacheExpress());
+app.set('view engine', 'ejs');
+
 app.set('views', __dirname + '/views');
-// app.use("/", express.static(__dirname + '/public'));
+app.use("/", express.static(__dirname + '/public'));
 app.use(methodOver('_method'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.locals.login = req.isAuthenticated();
+    next();
+});
 
-app.set('view engine', 'html');
 
 app.use(session({
   secret: 'theTruthIsOutThere51',
@@ -37,7 +46,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-// require('./app/routes.js')(app, passport);
+require('./app/routes.js')(app, passport);
 
 app.listen(port);
 console.log('server running its services in port', port);
